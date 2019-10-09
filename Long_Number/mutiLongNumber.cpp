ef42a38ef28781503dc7ebb61f2b129119d6b56f
NguyenTichDuy <nguyentichduy10a15cmg@gmail.com>
2019-10-09 12:22:20 +0700

    +Finish file cpp mutilongnumber.cpp

--- Long_Number/mutiLongNumber.cpp
+++ Long_Number/mutiLongNumber.cpp
@@ -1,8 +1,48 @@
 #include<iostream>
+#include<string.h>
 #include<string>
 using namespace std;
 
 void multi(string str1, string str2)
 {
-    int maxLength = (str1.size() > str2.size()) ? str1.size() : str2.size(); 
+    int maxLength = str1.size() + str2.size();
+    int result[maxLength + 1], num1[str1.size()], num2[str2.size()];
+
+    memset(result, 0, sizeof(result));
+
+    for (int i = 0; i < str1.size(); i++)
+    {
+        num1[i] = str1[str1.size() - i - 1] - '0';
+    }
+    for (int i = 0; i < str2.size(); i++)
+    {
+        num2[i] = str2[str2.size() - i - 1] - '0';
+    }
+
+    for (int i = 0; i < str1.size(); i++)
+    {
+        for (int j = 0; j < str2.size(); j++)
+        {
+            result[i + j] += num1[i] * num2[j];
+        }
+    }
+    int temp = 0;
+    for (int i = 0; i < maxLength + 1; i++)
+    {
+        result[i] = result[i]  + temp;
+        temp = result[i] / 10;
+        result[i] %= 10;
+    }
+    for (int i = 0; i < maxLength + 1; i++)
+    {
+        cout << result[maxLength - i];
+    }
+
+}
+int main()
+{
+    string a, b;
+    a = "5424252";
+    b = "4354";
+    multi(a, b);
 }
